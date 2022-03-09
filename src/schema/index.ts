1058be588ec6155d22a6f8d87cd4142e9c706beb
Scott <scott.olafsen@gmail.com>
2022-03-09 11:14:06 -0700

    source code

---
+++ src/schema/index.ts
@@ -0,0 +1,11 @@
+import { buildSchemaSync, Resolver, Query } from "type-graphql";
+import { ImageResolver } from "./image";
+import { HouseResolver } from "./house";
+import { authChecker } from "./auth";
+
+export const schema = buildSchemaSync({
+  // what are querys and mutations
+  resolvers: [ImageResolver, HouseResolver],
+  emitSchemaFile: process.env.NODE_ENV === "development",
+  authChecker,
+});
